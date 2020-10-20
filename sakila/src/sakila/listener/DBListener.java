9668f6d7a08e7aaa35d3a071cd693153264b7510
1F24DCA <fstflr446712@gmail.com>
2020-10-20 11:24:18 +0900

    프로젝트가 톰캣에 올라갈 때, JDBC 드라이버를 로드하는 리스너 생성
    이 리스너가 정상적으로 실행된다면, Class.forName("org.mariadb.jdbc.Driver")를 호출할 필요가
    없어짐

---
+++ sakila/src/sakila/listener/DBListener.java
@@ -0,0 +1,30 @@
+package sakila.listener;
+
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+import javax.servlet.annotation.WebListener;
+
+@WebListener
+public class DBListener implements ServletContextListener {
+	public void contextDestroyed(ServletContextEvent arg0)  {
+		// TODO Auto-generated method stub
+	}
+	
+	public void contextInitialized(ServletContextEvent arg0)  {
+		System.out.println("debug: method-begin: DBListener.contextInitialized()");
+
+		
+		try {
+			System.out.println("debug: message: 'loading org.mariadb.jdbc.Driver...'");
+			Class.forName("org.mariadb.jdbc.Driver");
+			
+			System.out.println("debug: message: 'load successfully: Login required'");
+		} catch (Exception e) {
+			e.printStackTrace();
+			
+			System.out.println("debug: message: 'load failed: org.mariadb.jdbc.Driver' ");
+		}
+		
+		System.out.println("debug: method-end: DBListener.contextInitialized()");
+	}
+}
